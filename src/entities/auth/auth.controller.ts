af76978f95ed382c021d021daef43fd15d3dddd6
Dmitry Surdin <dsurdin@eyeconweb.com>
2025-01-07 01:53:13 +0400

    refactoring

--- src/entities/auth/auth.controller.ts
+++ src/entities/auth/auth.controller.ts
@@ -37,6 +37,7 @@ const login = async (req: Request, res: Response): Promise<void> => {
 		res.cookie('refreshToken', refreshToken, {
 			httpOnly: true,
 			secure: true,
+			maxAge: 20000,
 		});
 
 		res.status(200).json({ accessToken });
@@ -90,6 +91,7 @@ const refreshToken = async (req: Request, res: Response): Promise<void> => {
 		res.cookie('refreshToken', newRefreshToken, {
 			httpOnly: true,
 			secure: true,
+			maxAge: 20000,
 		});
 
 		res.status(200).json({ accessToken: newAccessToken });
